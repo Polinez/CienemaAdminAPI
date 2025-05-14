f5918ac912d34f418b4aada805357aa1a27aacc0
Sebastian <sebastianwandzel@yahoo.pl>
2025-05-14 12:14:50 +0200

    feat: First commit

---
+++ src/main/java/com/example/cienemaadminapi/CienemaAdminApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.cienemaadminapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CienemaAdminApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CienemaAdminApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cienemaadminapi/CienemaAdminApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cienemaadminapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CienemaAdminApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
