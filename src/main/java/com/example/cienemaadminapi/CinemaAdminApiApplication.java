96fda47ee8b288c12efa3d1850a1b9b79db937a2
walkaroundthestars <kamila.przybylska@edu.uekat.pl>
2025-05-15 14:10:30 +0200

    adding some files to organize the project

--- src/main/java/com/example/cienemaadminapi/CinemaAdminApiApplication.java
+++ src/main/java/com/example/cienemaadminapi/CinemaAdminApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CienemaAdminApiApplication {
+public class CinemaAdminApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CienemaAdminApiApplication.class, args);
+        SpringApplication.run(CinemaAdminApiApplication.class, args);
     }
 
 }
