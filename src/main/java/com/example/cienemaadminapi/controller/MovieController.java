d36e335889ac037562ccba3726e636460d0166d0
Sebastian <sebastianwandzel@yahoo.pl>
2025-05-22 11:28:30 +0200

    feat: added controler to find all movies

--- src/main/java/com/example/cienemaadminapi/controller/MovieController.java
+++ src/main/java/com/example/cienemaadminapi/controller/MovieController.java
@@ -1,8 +1,13 @@
 package com.example.cienemaadminapi.controller;
 
+import com.example.cienemaadminapi.model.Movie;
+import com.example.cienemaadminapi.services.MovieService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
@@ -10,7 +15,7 @@ public class MovieController {
 
     @GetMapping("/movies")
     public String movies(Model model) {
-        List<Movie> movies = movieService.findAllMovies();
+        List<Movie> movies = MovieService.findAllMovies();
         model.addAttribute("movies", movies);
         return "movies";
     }
